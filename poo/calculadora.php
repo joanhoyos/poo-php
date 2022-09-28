84c6d6e1f28ed7ef198d1d9fd4b904472bbb6666
joan sebastian hoyos <35968341+joanhoyos@users.noreply.github.com>
2022-09-28 11:14:15 -0500

    Update calculadora.php

--- poo/calculadora.php
+++ poo/calculadora.php
@@ -4,34 +4,26 @@
 class Calculadora 
 {
 
-public function suma($numero1,$numero2);
-{
-	$suma = $numero1 + $numero2;
-	echo "la suma es: ". $suma . "<br>" ;
-} 
+	public function suma($numero1,$numero2)
+	{
+		$suma = $numero1 + $numero2;
+		echo "la suma es: ". $suma . "<br>";
+	} 
 
+	public function resta($numero1,$numero2)
+	{
+		$resta = $numero1 + $numero2;
+		echo "la resta es: ". $resta . "<br>";
+	} 
 
-public function resta($numero1,$numero2);
-{
-	$resta = $numero1 - $numero2;
-	echo "la resta es: ". $resta . "<br>" ;
-} 
+	public function multiplicacion($numero1,$numero2){
+		$multiplicacion = $numero1 * $numero2;
+		echo "la multiplicacion es: ". $multiplicacion . "<br>";
+	}
 
-public function multiplicacion($numero1,$numero2);
-{
-	$multiplicacion = $numero1 * $numero2;
-	echo "la multiplicacion es: ". $multiplicacion . "<br>" ;
-} 
-
-public function divicion($numero1,$numero2);
-{
-	$divicion = $numero1 + $numero2;
-	echo "la divicion es: ". $divicion . "<br>" ;
-} 
-
-
-
-
-
+	public function division($numero1,$numero2){
+		$division = $numero1 + $numero2;
+		echo "la division es: ". $division . "<br>";
+	} 
 }
- ?>
+?>
